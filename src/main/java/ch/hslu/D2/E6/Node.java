e70ca187b5b31696f04bcc16bd6175731e46b950
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-03-12 22:45:56 +0100

    removed not used override

--- src/main/java/ch/hslu/D2/E6/Node.java
+++ src/main/java/ch/hslu/D2/E6/Node.java
@@ -55,22 +55,4 @@ public class Node {
                 ", KindRechts=" + KindRechts +
                 '}';
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-        Node node = (Node) obj;
-        return Data == node.Data;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + Data;
-        return result;
-    }
 }
