8ec6b0576a31561e89275ce121f81fb9ec049196
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-03-25 14:58:14 +0100

    changed datatype

--- src/main/java/ch/hslu/D3/E1/Hashtabelle.java
+++ src/main/java/ch/hslu/D3/E1/Hashtabelle.java
@@ -2,7 +2,7 @@ package ch.hslu.D3.E1;
 
 public interface Hashtabelle {
 
-    void insert(int key, String value);
+    void insert(int key, Integer value);
     String search(int key);
     void remove(int key);
 
---
+++ src/main/java/ch/hslu/D3/E1/HashtabelleImpl.java
@@ -0,0 +1,18 @@
+package ch.hslu.D3.E1;
+
+public class HashtabelleImpl implements Hashtabelle{
+    @Override
+    public void insert(int key, Integer value) {
+
+    }
+
+    @Override
+    public String search(int key) {
+        return null;
+    }
+
+    @Override
+    public void remove(int key) {
+
+    }
+}
