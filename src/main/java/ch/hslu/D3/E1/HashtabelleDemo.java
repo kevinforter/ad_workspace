34adb8ea5c5b7c1e10e42cad461cfa6ad7a50141
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-03-26 15:24:52 +0100

    added insert and search

---
+++ src/main/java/ch/hslu/D3/E1/HashtabelleDemo.java
@@ -0,0 +1,10 @@
+package ch.hslu.D3.E1;
+
+public class HashtabelleDemo {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
--- src/main/java/ch/hslu/D3/E1/HashtabelleImpl.java
+++ src/main/java/ch/hslu/D3/E1/HashtabelleImpl.java
@@ -1,18 +1,52 @@
 package ch.hslu.D3.E1;
 
-public class HashtabelleImpl implements Hashtabelle{
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+import static java.util.Objects.hash;
+
+public class HashtabelleImpl implements Hashtabelle {
+
+    private final Integer[] arr;
+
+    public HashtabelleImpl() {
+        this.arr = new Integer[10];
+    }
+
+
     @Override
     public void insert(int key, Integer value) {
-
+        final int index = abs(hash(key) % arr.length);
+        arr[index] = value;
     }
 
     @Override
     public String search(int key) {
-        return null;
+        final int index = abs(hash(key) % arr.length);
+        return arr[index] != null ? arr[index].toString() : null;
     }
 
     @Override
     public void remove(int key) {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HashtabelleImpl that)) return false;
+        return Arrays.equals(arr, that.arr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(arr);
+    }
+
+    @Override
+    public String toString() {
+        return "HashtabelleImpl{" +
+                "arr=" + Arrays.toString(arr) +
+                '}';
     }
 }
