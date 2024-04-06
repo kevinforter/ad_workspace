53ec91719c082c787971eeba27a17a0401af0a9b
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-07 00:57:56 +0200

    used better logger

--- src/main/java/ch/hslu/D3/E2/PerformanceCompareDemo.java
+++ src/main/java/ch/hslu/D3/E2/PerformanceCompareDemo.java
@@ -1,10 +1,11 @@
 package ch.hslu.D3.E2;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PerformanceCompareDemo {
 
-    public static final Logger LOG = Logger.getLogger(PerformanceCompare.class.getName());
+    public static final Logger LOG = LoggerFactory.getLogger(PerformanceCompare.class);
 
     public static void main(String[] args) {
 
