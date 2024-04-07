e02b7116db11bcb7e3268c697c711959800fe058
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-07 18:36:29 +0200

    added Demo file

---
+++ src/main/java/ch/hslu/informatik/ad/datenstrukturen/D3/E3/Geeks4Geeks/HashtabelleDemoG4G.java
@@ -0,0 +1,19 @@
+package ch.hslu.informatik.ad.datenstrukturen.D3.E3.Geeks4Geeks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HashtabelleDemoG4G {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HashtabelleG4G.class);
+
+
+    public static void main (String[] args) {
+
+        HashtabelleG4G hashtable = new HashtabelleImplG4G(10);
+
+
+
+
+    }
+}
