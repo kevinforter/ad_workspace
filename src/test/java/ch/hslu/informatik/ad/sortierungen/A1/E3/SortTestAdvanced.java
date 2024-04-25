cf22f6a65661eabe83097200e459ce7d9e12e822
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-25 19:48:57 +0200

    added Logger and test case

--- src/test/java/ch/hslu/informatik/ad/sortierungen/A1/E3/SortTestAdvanced.java
+++ src/test/java/ch/hslu/informatik/ad/sortierungen/A1/E3/SortTestAdvanced.java
@@ -1,9 +1,17 @@
 package ch.hslu.informatik.ad.sortierungen.A1.E3;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SortTestAdvanced {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SortTestAdvanced.class);
 
     @BeforeAll
     static void setUp() {
@@ -13,5 +21,16 @@ public class SortTestAdvanced {
     @AfterAll
     static void tearDown() {
 
+    }
+
+    @Ignore
+    void test_return() {
+
+        int[] unsortArr = new int[]{19, 75, 67, 21, 9};
+
+        int[] sortedArr = Sort.bubbleSort(unsortArr);
+        assertEquals(5, sortedArr.length);
+
+        LOG.info(Arrays.toString(sortedArr));
     }
 }
