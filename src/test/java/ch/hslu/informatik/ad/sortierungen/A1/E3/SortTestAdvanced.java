7a276813ef3d0071bc1cadc91ea7eeba8c34eadc
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-25 15:02:07 +0200

    made new test File and standard methods

---
+++ src/test/java/ch/hslu/informatik/ad/sortierungen/A1/E3/SortTestAdvanced.java
@@ -0,0 +1,17 @@
+package ch.hslu.informatik.ad.sortierungen.A1.E3;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SortTestAdvanced {
+
+    @BeforeAll
+    static void setUp() {
+
+    }
+
+    @AfterAll
+    static void tearDown() {
+
+    }
+}
