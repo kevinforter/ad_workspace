5e17d95a03ad423ced4617ac4dbaabf12aa6fbec
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-25 19:58:51 +0200

    added parameter

--- src/test/java/ch/hslu/informatik/ad/sortierungen/A1/E3/SortTestAdvanced.java
+++ src/test/java/ch/hslu/informatik/ad/sortierungen/A1/E3/SortTestAdvanced.java
@@ -2,10 +2,15 @@ package ch.hslu.informatik.ad.sortierungen.A1.E3;
 
 import org.junit.Ignore;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,14 +28,19 @@ public class SortTestAdvanced {
 
     }
 
-    @Ignore
-    void test_return() {
-
-        int[] unsortArr = new int[]{19, 75, 67, 21, 9};
+    @ParameterizedTest
+    @MethodSource("provideArrays")
+    void test_return(int[] unsortArr) {
 
         int[] sortedArr = Sort.bubbleSort(unsortArr);
         assertEquals(5, sortedArr.length);
 
         LOG.info(Arrays.toString(sortedArr));
+    }
+
+    static Stream<Arguments> provideArrays() {
+        return Stream.of(
+                Arguments.of(new int[]{19, 75, 67, 21, 9})
+        );
     }
 }
