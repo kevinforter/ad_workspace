a662081947d87855ee4971ac47c9f9d8121cc39a
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-01 00:02:00 +0200

    added quickSort

--- src/main/java/ch/hslu/informatik/ad/sortierungen/A2/E2/Sort.java
+++ src/main/java/ch/hslu/informatik/ad/sortierungen/A2/E2/Sort.java
@@ -34,9 +34,9 @@ public class Sort {
     /**
      * Sortiert das int-Array aufsteigend mit Binär-Suche
      *
-     * @link <a href="https://www.geeksforgeeks.org/java-program-for-binary-insertion-sort/">Binary Insertion sort</a>
      * @param a Zu sortierendes Array.
      * @return sortiertes Array
+     * @link <a href="https://www.geeksforgeeks.org/java-program-for-binary-insertion-sort/">Binary Insertion sort</a>
      */
     public static int[] insertionSort3(final int[] a) {
         for (int i = 1; i < a.length; i++) {
@@ -80,9 +80,9 @@ public class Sort {
     /**
      * Sortiert das int-Array aufsteigend
      *
-     * @link <a href="https://www.geeksforgeeks.org/java-program-for-bubble-sort/">Bubble Sort</a>
      * @param a Zu sortierendes Array.
      * @return sortiertes Array
+     * @link <a href="https://www.geeksforgeeks.org/java-program-for-bubble-sort/">Bubble Sort</a>
      */
     public static int[] bubbleSort(final int[] a) {
         // Anzahl Elemente im Array abarbeiten (-1 weil Anz Vergleiche eins weniger als Elemente ist)
@@ -90,7 +90,7 @@ public class Sort {
 
             // Start immer beim ersten Element (j = 0)
             // -i weil rechter Teil immer sortiert ist
-            for (int j = 0; j < a.length -i - 1; j++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
 
                     // Tausch von a[j] und a[j+1]
@@ -106,9 +106,9 @@ public class Sort {
     /**
      * Sortiert das int-Array aufsteigend
      *
-     * @link <a href="https://www.geeksforgeeks.org/bubble-sort/">Bubble Sort with check</a>
      * @param a Zu sortierendes Array.
      * @return sortiertes Array
+     * @link <a href="https://www.geeksforgeeks.org/bubble-sort/">Bubble Sort with check</a>
      */
     public static int[] bubbleSort2(final int[] a) {
         // Anzahl Elemente im Array abarbeiten (-1 weil Anz Vergleiche eins weniger als Elemente ist)
@@ -116,7 +116,7 @@ public class Sort {
             boolean swapped = false;
             // Start immer beim ersten Element (j = 0)
             // -i weil rechter Teil immer sortiert ist
-            for (int j = 0; j < a.length -i - 1; j++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
 
                     // Tausch von a[j] und a[j+1]
@@ -131,5 +131,42 @@ public class Sort {
         return a;
     }
 
+    /**
+     * Vertauscht zwei bestimmte Zeichen im Array.
+     *
+     * @param a           Zeichen-Array
+     * @param firstIndex  Index des ersten Zeichens
+     * @param secondIndex Index des zweiten Zeichens
+     */
+    private static void exchange(final char[] a, final int firstIndex, final int secondIndex) {
+        char tmp;
+        tmp = a[firstIndex];
+        a[firstIndex] = a[secondIndex];
+        a[secondIndex] = tmp;
+    }
 
+    public static void quickSort(final char[] a, final int left, final int right) {
+        int up = left; // linke Grenze
+        int down = right - 1; // rechte Grenze (ohne Trennelement)
+        char t = a[right]; // rechtes Element als Trennelement
+        boolean allChecked = false;
+        do {
+            while (a[up] < t) {
+                up++; // suche grösseres (>=) Element von links an
+            }
+            while ((a[down] >= t) && (down > up)) {
+                down--; // suche echt kleineres (<) Element von rechts an
+            }
+            if (down > up) { // solange keine Überschneidung
+                exchange(a, up, down);
+                up++;
+                down--; // linke und rechte Grenze verschieben
+            } else {
+                allChecked = true; // Austauschen beendet
+            }
+        } while (!allChecked);
+        exchange(a, up, right); // Trennelement an endgültige Position (a[up])
+        if (left < (up - 1)) quickSort(a, left, (up - 1)); // linke Hälfte
+        if ((up + 1) < right) quickSort(a, (up + 1), right); // rechte Hälfte, ohne T’Elt.
+    }
 }
