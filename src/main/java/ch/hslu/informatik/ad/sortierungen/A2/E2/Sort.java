10e0d86a0d6cd33df49d585f142f2a45b5e7c02b
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-05 22:46:43 +0200

    optimiere den Quicksort, so dass auch Datenelemente, welche gleich wie das Trennelement sind vertauscht werden

--- src/main/java/ch/hslu/informatik/ad/sortierungen/A2/E2/Sort.java
+++ src/main/java/ch/hslu/informatik/ad/sortierungen/A2/E2/Sort.java
@@ -146,29 +146,29 @@ public class Sort {
     }
 
     public static void quickSort(final char[] a, final int left, final int right) {
-        int up = left; // linke Grenze
-        int down = right - 1; // rechte Grenze (ohne Trennelement)
-        char t = a[right]; // rechtes Element als Trennelement
-        boolean allChecked = false;
-        do {
-            while (a[up] < t) {
-                up++; // suche grösseres (>=) Element von links an
-            }
-            while ((a[down] >= t) && (down > up)) {
-                down--; // suche echt kleineres (<) Element von rechts an
-            }
-            if (down > up) { // solange keine Überschneidung
-                exchange(a, up, down);
-                up++;
-                down--; // linke und rechte Grenze verschieben
-            } else {
-                allChecked = true; // Austauschen beendet
-            }
-        } while (!allChecked);
-        exchange(a, up, right); // Trennelement an endgültige Position (a[up])
-        if (left < (up - 1)) quickSort(a, left, (up - 1)); // linke Hälfte
-        if ((up + 1) < right) quickSort(a, (up + 1), right); // rechte Hälfte, ohne T’Elt.
-    }
+    int up = left; // linke Grenze
+    int down = right - 1; // rechte Grenze (ohne Trennelement)
+    char t = a[right]; // rechtes Element als Trennelement
+    boolean allChecked = false;
+    do {
+        while (a[up] < t) {
+            up++; // suche grösseres (>=) Element von links an
+        }
+        while ((a[down] > t) && (down > up)) { // geänderte Bedingung
+            down--; // suche echt kleineres (<) Element von rechts an
+        }
+        if (down > up) { // solange keine Überschneidung
+            exchange(a, up, down);
+            up++;
+            down--; // linke und rechte Grenze verschieben
+        } else {
+            allChecked = true; // Austauschen beendet
+        }
+    } while (!allChecked);
+    exchange(a, up, right); // Trennelement an endgültige Position (a[up])
+    if (left < (up - 1)) quickSort(a, left, (up - 1)); // linke Hälfte
+    if ((up + 1) < right) quickSort(a, (up + 1), right); // rechte Hälfte, ohne T’Elt.
+}
 
     public static void quickSort(final char[] a) {
         int left = 0;
