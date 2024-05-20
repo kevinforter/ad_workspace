5ea646cc6bfab02996fb0ac1fa4e64a20cf434c6
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-20 15:55:40 +0200

    added A4

---
+++ src/main/java/ch/hslu/informatik/ad/suchen/A4/E2/OptimizedSearch.java
@@ -0,0 +1,12 @@
+package ch.hslu.informatik.ad.suchen.A4.E2;
+
+public class OptimizedSearch {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int stateSearch(final String a) {
+        return 0;
+    }
+}
