08a0fa8a99c62810d81d20a5ccde3364482de96c
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-31 22:42:15 +0200

    added fiboRec2

--- src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E4/FibonacciDemo.java
+++ src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E4/FibonacciDemo.java
@@ -2,23 +2,42 @@ package ch.hslu.informatik.ad.einfuehrung.E1.E4;
 
 public class FibonacciDemo {
 
+    static int[] f = new int[1024];
+
     public static void main(String[] args) {
-        int i = fiboRec1(9);
-        System.out.println(i);
+        int fib1 = fiboRec1(9);
+        System.out.println(fib1);
+
+        int fib2 = fiboRec2(9);
+        System.out.println(fib2);
     }
 
     public static int fiboRec1(int n) {
         int f = 1;
 
+        // Rekursionsbasis
         if (n == 0) {
             return 0;
         }
 
+        // Rekursionsbasis
         if (n == 1) {
             return f;
         }
 
+        // Rekursionsvorschrift
         f = fiboRec1(n - 2) + fiboRec1(n - 1);
         return f;
+    }
+
+    public static int fiboRec2(int n) {
+
+        if(n == 0 || n == 1) {
+            return n;
+        }
+
+        f[n] = fiboRec1(n - 2) + fiboRec1(n - 1);
+        return f[n];
+
     }
 }
