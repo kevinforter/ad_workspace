6056f8890fa76e72b6afb09cdf0158f77c974772
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-31 23:30:51 +0200

    added FiboIter

--- src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E4/FibonacciDemo.java
+++ src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E4/FibonacciDemo.java
@@ -10,6 +10,9 @@ public class FibonacciDemo {
 
         int fib2 = fiboRec2(9);
         System.out.println(fib2);
+
+        int fib3 = fiboIter(9);
+        System.out.println(fib3);
     }
 
     public static int fiboRec1(int n) {
@@ -32,16 +35,30 @@ public class FibonacciDemo {
 
     public static int fiboRec2(int n) {
 
-        if(n == 0 || n == 1) {
+        if (n == 0 || n == 1) {
             return n;
         }
 
-        if(f[n] != 0) {
+        if (f[n] != 0) {
             return f[n];
         }
 
         f[n] = fiboRec1(n - 2) + fiboRec1(n - 1);
         return f[n];
+    }
 
+    public static int fiboIter(int n) {
+
+        if (n == 0) return n;
+
+        int ppN, pN = 0, cN = 1;
+
+        for (int i = 1; i < n; i++) {
+            ppN = pN;
+            pN = cN;
+            cN = ppN + pN;
+        }
+
+        return cN;
     }
 }
