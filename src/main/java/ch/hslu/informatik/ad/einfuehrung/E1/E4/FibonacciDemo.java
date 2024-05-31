c9c678468c8d364e0d797d4d93167f4d508598a8
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-31 22:10:16 +0200

    added fiboRec1 [E4]

--- src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E4/FibonacciDemo.java
+++ src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E4/FibonacciDemo.java
@@ -1,4 +1,24 @@
 package ch.hslu.informatik.ad.einfuehrung.E1.E4;
 
 public class FibonacciDemo {
+
+    public static void main(String[] args) {
+        int i = fiboRec1(9);
+        System.out.println(i);
+    }
+
+    public static int fiboRec1(int n) {
+        int f = 1;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return f;
+        }
+
+        f = fiboRec1(n - 2) + fiboRec1(n - 1);
+        return f;
+    }
 }
