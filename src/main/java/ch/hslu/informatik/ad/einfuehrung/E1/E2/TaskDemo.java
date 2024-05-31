67b331bc6d18d8f30059b383cb6d73ff759b2dd6
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-31 03:42:13 +0200

    changed input stream

--- src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E2/TaskDemo.java
+++ src/main/java/ch/hslu/informatik/ad/einfuehrung/E1/E2/TaskDemo.java
@@ -10,7 +10,7 @@ public class TaskDemo {
 
     public static void main(String[] args) {
 
-        Stream<Integer> n = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 40);
+        Stream<Integer> n = Stream.of(1, 2, 10, 20, 40, 80);
 
         n.forEach(i -> {
             try {
