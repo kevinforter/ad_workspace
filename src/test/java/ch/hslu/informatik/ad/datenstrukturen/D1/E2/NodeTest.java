9f4148b90ecfa86bae5e5355399c048c3449b0bb
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-08-07 00:10:36 -0700

    added nodeTest

---
+++ src/test/java/ch/hslu/informatik/ad/datenstrukturen/D1/E2/NodeTest.java
@@ -0,0 +1,40 @@
+package ch.hslu.informatik.ad.datenstrukturen.D1.E2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mutabilitydetector.unittesting.MutabilityAssert.assertImmutable;
+
+class NodeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void getNext() {
+    }
+
+    @Test
+    void setNext() {
+    }
+
+    @Test
+    void testImmutability() {
+        assertImmutable(LinkedList.class);
+    }
+}
